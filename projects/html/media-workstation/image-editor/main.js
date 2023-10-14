372cbfdebe74b2c1c40ea31a639de5b3fbbbc0f2
Quitin <53788234+Quitin@users.noreply.github.com>
2023-10-14 22:10:41 +0300

    image editor update

--- projects/html/media-workstation/image-editor/main.js
+++ projects/html/media-workstation/image-editor/main.js
@@ -2,10 +2,9 @@ $ = (x) => {return document.getElementById(x+"")};
 
 c = $('display');
 ctx = $('display').getContext('2d', {willReadFrequently: true});
-var auto = true;
-var effects = [];
-var seindex = 0;
-var efscc, selectedelement
+var auto = true, effects = [], seindex = 0, efscc, selectedelement, V = 1, g = !1, vd = g
+let autorender
+l = localStorage
 
 class Effect {
     constructor(name, effect, bypass) {
@@ -109,10 +108,12 @@ function loadImg() { // this initializes the source image and the image for the 
 
 }
 
-function newCanvas(w,h) { // creates an empty canvas of any size
+function newCanvas(w,h,src) { // creates an empty canvas of any size
+    c.width = 256;
+    c.height = 256;
     imgd = ctx.getImageData(0,0,c.width,c.height)
     img = new Image(w,h);
-    img.src = '';
+    img.src = src || '';
     // img.src = '../image-editor/images/disguised face.png' <- uncomment this for the disguised face haha lol Functinos.
         ctx.clearRect(0,0,c.width,c.height);
         ctx.drawImage(img,0,0,img.width,img.height,0,0,c.width,c.height);
@@ -138,7 +139,14 @@ function updateCSS() { // this function asserts that all of the image can be see
 window.onresize = updateCSS
 
 loadImg()
-setInterval(()=>{try{auto?render(true):0}catch{}},10) // Execute scripts automatically (by default) if auto is true.
+setAR()
+
+$('s-auto').oninput = setAR
+
+function setAR() {
+    clearInterval(autorender)
+    autorender = setInterval(()=>{try{auto?render(true):0}catch{}},g?500:10) // Execute scripts automatically (by default) if auto is true.
+}
 
 function getPixel(x,y) { // gets the rgba data of the specified pixel from the source (or the image the code is currently applying effects to)
     let i = (Math.floor(x) - width*Math.floor(x/width)) + (Math.floor(y) - width*Math.floor(y/width)) * width
@@ -146,7 +154,7 @@ function getPixel(x,y) { // gets the rgba data of the specified pixel from the s
 }
 
 function renameeffect() {
-    let newname = prompt('Set the new name to...');
+    let newname = prompt('Set the new name to...',effects[seindex].name);
     if (!!newname) {
         $('effectsc').children[seindex].children[0].innerHTML = newname;
         effects[seindex].name = newname;
@@ -243,6 +251,15 @@ function render(takefromEffects,...efs) {
     if (takefromEffects) {
         efs = effects.map((v)=>{return v.effect})
     };
+    if (g) {
+        const frames = $('frames').value
+        var progress = round((V+1) / frames * 1e4) / 100
+        gifenc.addFrame(ctx, false); V++
+        $('infobox').innerHTML = '<p>' + `Rendering ${V} / ${frames} frames (${progress} %)` + '<button style="display:inline" onclick=stopexport()>Cancel</button>' + '</p>'
+    };
+    if (vd) {
+        gifenc.addFrame(ctx, false); V++
+    };
     ctx.clearRect(0,0,c.width,c.height);
     ctx.drawImage(img,0,0,img.width,img.height,0,0,c.width,c.height);
     image = ctx.getImageData(0,0,c.width,c.height);
@@ -299,7 +316,6 @@ function newEffect(...x) {
 
 $('upload').oninput = function() {
     file = $('upload').files[0];
-    console.log('Did it go well? Hmmm...');
     if (file) {
         reader = new FileReader();
             reader.onload = function(event) {  
@@ -336,4 +352,219 @@ function switchPage(x) {
 
 }
 
-switchPage(0)
+function saveImage() { // saves canvas image to local machine
+    let downloadLink = document.createElement('a');
+    downloadLink.setAttribute('download', 'Image_Editor_Saved_Image.png');
+    $('display').toBlob(function(blob) {
+        let url = URL.createObjectURL(blob);
+        downloadLink.setAttribute('href', url);
+        downloadLink.click();
+    });
+    $('infobox').innerHTML = '<p class="white">Saved image! The image is downloading to your local machine, please wait.</p>'
+    if (typeof fade !== 'undefined' && typeof fade !== 'undefined') {clearTimeout(fade); clearTimeout(fade2)}
+    fade = setTimeout(()=>{
+        $('infobox').children[0].style.opacity = '0%'
+        fade2 = setTimeout(()=>{
+            $('infobox').innerHTML = ''
+        },500)
+    },5e3)
+}
+
+function copyImage() { // copies canvas image to clipboard
+    $('display').toBlob(function(blob) { 
+        const item = new ClipboardItem({ "image/png": blob });
+        navigator.clipboard.write([item]); 
+    });
+    $('infobox').innerHTML = '<p class="green">Copied to clipboard!</p>'
+    if (typeof fade !== 'undefined' && typeof fade !== 'undefined') {clearTimeout(fade); clearTimeout(fade2)}
+    fade = setTimeout(()=>{
+        $('infobox').children[0].style.opacity = '0%'
+        fade2 = setTimeout(()=>{
+            $('infobox').innerHTML = ''
+        },500)
+    },3e3)
+}
+
+setInterval(() => {
+    $('export').style.color = `hsl(${(Date.now())/20},100%,60%)`
+    $('export').style.backgroundColor = `hsl(${(Date.now())/20},50%,5%)`
+},30)
+
+switchPage(1)
+
+function exportgif() {
+    g = !0
+    setAR()
+    gifenc = new GIFEncoder()
+    gifenc.setSize(c.width,c.height)
+    gifenc.setDelay(1e3 / ($('fps').value * 1 || 30))
+    gifenc.setQuality($('quality').value || 32)
+    gifenc.setRepeat(0)
+    auto = true
+    gifenc.start()
+    rendering = setInterval(()=>{
+        if (V >= (($('frames').value * 1) || 100)) {
+            gifenc.finish()
+            g = !1, V = 1, auto = false
+            vg = gifenc;
+            gifenc.download($('gifname').value || 'Untitled')
+            clearInterval(rendering)
+            $('infobox').innerHTML = '<p class="green">Render complete!</p>'
+        }
+    },10)
+}
+
+function stopexport() {
+    gifenc.finish()
+    g = !1, V = 1, auto = false
+    vg = gifenc;
+    clearInterval(rendering)
+    $('infobox').innerHTML = ''
+}
+
+// This is a scrapped (and unfinished) feature.
+// I couldn't find a working library that can create videos.
+// I've tried stuff such as FFmpeg.js, MediaRecorder, VideoContext.js...
+// And none of them fitted my needs.
+
+/* function exportvideo() {
+    vd = !0
+    videnc = new Whammy.Video(30,10)
+    videnc.setSize(c.width,c.height)
+    auto = true
+    gifenc.start()
+    let wait = setInterval(()=>{
+        if (V >= 60) {
+            videnc.compile()
+            vd = !1, V = 1
+            gifenc.download('Test')
+            clearInterval(wait)
+        }
+    },10)
+} */ 
+
+function saveSession() {
+    l.setItem('effects', JSON.stringify(effects))
+    l.setItem('image',img.src+'')
+    l.setItem('imgdims','['+[img.width,img.height]+']')
+}
+
+function loadSession() {
+    let [w1,w2] = JSON.parse(l.getItem('imgdims'));
+    let src2 = l.getItem('image')
+    img.src = src2
+    newCanvas(w1,w2,src2)
+    effects = JSON.parse(l.getItem('effects'))
+    displayAllEffects()
+    selectEffect(0)
+}
+
+function updateInfo() {
+    let r = (x) => {return round(x * 100) / 100}
+    let Ts = $('frames').value / $('fps').value
+    let Tmin = Ts / 60
+    let Th = Tmin / 60
+    var Length = ''
+
+    Length = '= ' + r(Ts) + ' seconds';
+    if (Tmin >= 1) {Length = '= ' + r(Tmin) + ' minutes'}
+    if (Th >= 1) {Length = '= ' + r(Th) + ' hours'}
+    $('calclength').innerHTML = Length
+}
+
+function fitImage(b3) {
+    let a1 = $('right-half');
+    let a2 = a1.style;
+    if (b3) {  
+        let a = a1.getBoundingClientRect()
+        let b1 = a.width / c.width
+        let b2 = a.height / c.height
+        let d = min(b1,b2) * .75
+        $('display').style.transform = `scale(${d})`
+        a1.scrollTop = 0
+        a1.scrollLeft = 0
+        a2.overflow = 'hidden'
+    } else {
+        $('display').style.transform = ''
+        a2.overflow = ''
+    }
+}
+
+function tick() {
+    updateInfo()
+    updateCSS()
+    fitImage($('s-fitimage').checked)
+}
+
+function writeFile(content,name) {
+    const filename = name+'.txt';
+  
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = window.URL.createObjectURL(blob);
+  
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    link.click();
+  
+    window.URL.revokeObjectURL(url);
+}
+
+$('upload-project').addEventListener('change', handleFile)
+
+function handleFile() {
+    const fileInput = $('upload-project');
+    const file = fileInput.files[0];
+  
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function(event) {
+        debugger
+        const contents = event.target.result;
+        let contents1 = contents.split('#DELIMITER#')
+        let parsedcontents = contents1[1], parsedeffects = contents1[0];
+        try {JSON.parse(parsedcontents)+0} catch {parsedcontents = 'Error'};
+        try {JSON.parse(parsedeffects)+0} catch {parsedeffects = 'Error'};
+        if (parsedcontents != 'Error') {
+        parsedeffects = JSON.parse(contents1[0])
+        parsedcontents = JSON.parse(contents1[1])
+        };
+        let p1 = parsedcontents, p2 = parsedeffects;
+        globaltestp = [p1, p2]
+        console.log(parsedcontents)
+        if (Array.isArray(p1) && typeof p2 == 'object' && p1.length == 3) {
+            img.src = p1[2]
+            newCanvas(p1[0], p1[1], p1[2])
+            effects = p2
+            displayAllEffects()
+            selectEffect(0)
+        } else {
+            console.log('This file is not a valid preset file.');
+        }
+      };
+      reader.readAsText(file);
+    } else {
+      console.log('No file selected.');
+    }
+  }
+
+function saveProject() {
+    let [i1,iw,ih] = [img.src, c.width, c.height];
+    let efs = JSON.stringify(effects)
+    let cn = efs+
+        '\n#DELIMITER#\n'+
+        '[\n'+
+        '"'+iw+'",\n'+
+        '"'+ih+'",\n'+
+        '"'+i1+'"\n]';
+    globalCn = cn
+    writeFile(cn,$('project-name').value)
+}
+
+setTimeout(loadSession,1e2)
+
+newCanvas(width,height)
+
+setInterval(saveSession,10e3)
+
+setInterval(tick,50/3)
